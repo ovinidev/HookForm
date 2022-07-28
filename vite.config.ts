b2d678e1781b554bb45049b48bafb1e1352fae8e
vinimedeiros13 <vinimedeiros7@outlook.com>
2022-07-28 14:43:41 -0300

    feat: obfuscate code

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteObfuscateFile } from 'vite-plugin-obfuscator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteObfuscateFile({
-
-  })]
+  plugins: [react()]
 })
